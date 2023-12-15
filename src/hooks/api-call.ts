505d1cd3b45e1ce6ca2b5469f17276632d0d17f7
itscreek <uraitsu2000@gmail.com>
2023-12-15 17:06:05 +0900

    Remove temporary error handling

--- src/hooks/api-call.ts
+++ src/hooks/api-call.ts
@@ -1,8 +1,7 @@
-export interface TsuriScore {
+export interface TsuriScoreItem {
   status: string;
   video_id: string;
   tsuri_score: number;
-  tsuri_report: any;
 }
 
 export interface TsuriScoreAPIRequest {
@@ -10,22 +9,16 @@ export interface TsuriScoreAPIRequest {
 }
 
 export interface TsuriScoreAPIResponse {
-  items: TsuriScore[];
+  items: TsuriScoreItem[];
 }
 
 const API_URL = 'http://localhost:40000/v1';
 
 // API call function
-export const tsuriScoreAPICall = async (input: TsuriScoreAPIRequest): Promise<TsuriScoreAPIResponse> => {
+export const tsuriScoreAPI = async (input: TsuriScoreAPIRequest): Promise<TsuriScoreAPIResponse> => {
   const inputVideoIds = input.video_ids.join(',');
   const url = `${API_URL}/report?video_id=${inputVideoIds}`;
 
   const response = await fetch(url);
-
-  if (!response.ok) {
-    // TODO: Error handling
-    throw new Error(`HTTP error! Status: ${response.status}`);
-  }
-
-  return await response.json();
+  return response.json();
 };
